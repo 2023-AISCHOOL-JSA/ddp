07ab46c7bdf4d3aab823c573781d8ca4981df6b8
김유열 <kyyyy8629@gmail.com>
2023-08-01 19:30:32 +0900

    ddp 프로젝트 첫 커밋

---
+++ ddp/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+// 클라이언트 요청정보얻기
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+// .env로 키관리
+const dotenv = require("dotenv");
+const path = require("path");
+// 이미지처리 라이브러리
+const multer = require("multer");
+// 파일처리
+const fs = require("fs");
+const router = require("./router/router");
+
+dotenv.config();
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.use(morgan("dev"));
+// 정적파일 path처리
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    httpOnly: true, // http 통신일때 허용
+    secret: "secretkey", // 암호화 키
+    resave: false, // 요청이 들어왔을 때 세션의 수정사항이 없더라도 다시저장
+  })
+);
+app.use(router);
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번시작");
+});
---
+++ ddp/router/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// 쿠키: 저장공간을 클라이언트로 이용해서 데이터를 관리하는 기술
+// 쿠키 생성
+router.get("/", (req, res) => {
+  console.log("기본경로");
+});
+
+module.exports = router;
